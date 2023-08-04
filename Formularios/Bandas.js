ff98b92035e4dc09a80daafdc3309f8841991b06
Lisset100 <liset10nieve@live.com.mx>
2023-08-03 19:46:46 -0600

    Funciones onclick

---
+++ Formularios/Bandas.js
@@ -0,0 +1,10 @@
+function Gracias() {
+    alert("Gracias, se ha guardado correctamente");
+  }
+function Graficado() {
+    alert("Gracias, se ha graficado correctamente");
+  }
+  function Borrado() {
+    alert("Gracias, se ha borrado correctamente");
+  }
+  
