852d75be9945ffe53053cd034fb3924472311f47
Lisset100 <liset10nieve@live.com.mx>
2023-08-04 20:21:55 -0600

    Formularios faltantes

---
+++ Formularios/Conciertos.js
@@ -0,0 +1,10 @@
+function Gracias() {
+    alert("Gracias, se ha guardado correctamente");
+  }
+function Graficado() {
+    alert("Se ha graficado correctamente");
+  }
+  function Borrado() {
+    alert("Se ha borrado correctamente");
+  }
+  
--- Formularios/Generos.js
+++ Formularios/Generos.js
@@ -2,9 +2,9 @@ function Gracias() {
     alert("Gracias, se ha guardado correctamente");
   }
 function Graficado() {
-    alert("Gracias, se ha graficado correctamente");
+    alert("Se ha graficado correctamente");
   }
   function Borrado() {
-    alert("Gracias, se ha borrado correctamente");
+    alert("Se ha borrado correctamente");
   }
   
