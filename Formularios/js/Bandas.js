ea7a107d1ee94a209c736a92127bdcaee1f94a28
Lisset100 <liset10nieve@live.com.mx>
2023-08-08 17:58:20 -0600

    feat: ultimosCambios

--- Formularios/js/Bandas.js
+++ Formularios/js/Bandas.js
@@ -1,39 +1,34 @@
 function Gracias() {
     alert("Gracias, se ha guardado correctamente");
-  }
+}
 function Graficado() {
     alert("Se ha graficado correctamente");
-  }
-  function Borrado() {
+}
+function Borrado() {
     alert("Se ha borrado correctamente");
-  }
-  function eliminar(id){
-  id=parseint(id);
-  endPoint="http://127.0.0.1:8000/api/bands/id"
-  }
-  function mostrarDatos(){
-    fetch("http://127.0.0.1:8000/api/bands")
+}
+function eliminar(id) {
+    id = parseint(id);
+    endPoint = "http://127.0.0.1:8000/api/bands/id";
+}
+function mostrarDatos() {
+    fetch("http://127.0.0.1:8000/api/bands");
 
-  onclick="eliminar"()
-  }
-  
-  
-  const selectElement = document.getElementById("selectOptions");
-            
-                // URL del endpoint de la API
-                const apiUrl = "http://127.0.0.1:8000/api/bands";
-            
-                // Realizar la solicitud a la API
-                fetch(apiUrl)
-                  .then(response => response.json())
-                  .then(data => {
-                    // Iterar sobre los datos y crear opciones para el select
-                    data.forEach(option => {
-                      const optionElement = document.createElement("option");
-                      optionElement.value = option.id;
-                      optionElement.textContent = option.nombre;
-                      selectElement.appendChild(optionElement);
-                    });
-                  })
-                  .catch(error => console.error("Error al cargar las opciones:", error));
-            
+    onclick = "eliminar"();
+}
+
+// Datos obtenidos de la respuesta de la API en Thunder Client
+const responseData = [
+    { id: 1, name: "Pink Floyd" },
+    { id: 2, name: "Iron Maiden" },
+    { id: 3, name: "AC/DC" },
+];
+
+const selectElement = document.getElementById("selectOptions");
+
+responseData.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option.id;
+    optionElement.textContent = option.nombre;
+    selectElement.appendChild(optionElement);
+});
--- Formularios/js/Generos.js
+++ Formularios/js/Generos.js
@@ -1,10 +1,11 @@
-function Gracias() {
-    alert("Gracias, se ha guardado correctamente");
-  }
+function Save() {
+    //alert("Gracias, se ha guardado correctamente");
+    const selectElement = document.getElementById("genderName");
+    console.log(selectElement.value);
+}
 function Graficado() {
     alert("Se ha graficado correctamente");
-  }
-  function Borrado() {
+}
+function Borrado() {
     alert("Se ha borrado correctamente");
-  }
-  
+}
